adb4871e50a7f3dd9c9cb3645b68cf4e366a392f
Aarif09 <105052361+Aarif09@users.noreply.github.com>
2025-02-06 14:06:04 +0530

    Add files via upload
    Assingment

--- Assignment!.sql
+++ Assignment!.sql
@@ -105,11 +105,28 @@ on pd.PurchaseOrderID = ph.PurchaseOrderID
 where day(ph.OrderDate)=day(ph.ShipDate)
 
 
+--7)find the average days required to ship the product based on shipment type.
+select* from Purchasing.ShipMethod
+select* from Production.Product
+select* from Purchasing.PurchaseOrderHeader
+select* from Purchasing.PurchaseOrderDetail
+
+
+select 
+    ps.Name as Shipment_Type, 
+    avg(DATEDIFF(DAY, ph.OrderDate, ph.ShipDate)) as Avg_Shipping_Days
+from Purchasing.PurchaseOrderHeader ph
+join Purchasing.ShipMethod ps 
+    on ph.ShipMethodID = ps.ShipMethodID
+where ph.ShipDate is not null
+group by ps.Name
+order by Avg_Shipping_Days desc;
+
 --8)find the name of employees working in day shift
 
 select CONCAT_WS(' ',FirstName,LastName)as Emp_name from Person.Person
 where BusinessEntityID in (select BusinessEntityID from HumanResources.EmployeeDepartmentHistory 
-where ShiftID in (select ShiftID from HumanResources.Shift where ShiftID=1))
+where ShiftID in (select ShiftID from HumanResources.Shift where Name='DAY'))
 
 --9.based on product and product cost history find the name ,
 --service provider time and average Standardcost
@@ -202,14 +219,18 @@ WHERE sp.TerritoryID IN (
 
 --15.Find all employees who worked in all North America territory
 
-select TerritoryID,
-(select concat(' ',FirstName,LastName)from Person.Person p where p.BusinessEntityID=sp.BusinessEntityID)Emp_Name,
+select  distinct TerritoryId,
+(select distinct concat(' ',FirstName,LastName)from Person.Person p where p.BusinessEntityID=sp.BusinessEntityID)Emp_Name,
 (select name  from Sales.SalesTerritory st where  st.TerritoryID=sp.TerritoryID) TerritoryName,
-(select [Group] from Sales.SalesTerritory st1 where st1.TerritoryID=sp.TerritoryID)Group_NAme,SalesLastYear,SalesQuota
-from Sales.SalesPerson sp WHERE sp.TerritoryID IN (
+(select [Group] from Sales.SalesTerritory st1 where st1.TerritoryID=sp.TerritoryID)Group_NAme
+from Sales.SalesTerritoryHistory sp WHERE sp.TerritoryID IN (
     SELECT TerritoryID 
     FROM Sales.SalesTerritory 
     WHERE [Group] IN ('North America'))
+
+
+
+
 
 --16.find all products in the cart
 Select (select Name from Production.Product pp where pp.ProductID=si.ProductID)Prod_name,
@@ -243,6 +264,372 @@ JOIN HumanResources.Employee e
 ON eph.BusinessEntityID = e.BusinessEntityID
 JOIN Person.Person p 
 ON e.BusinessEntityID = p.BusinessEntityID
+
+--with subQuery
+
+select 
+    (select p.FirstName from Person.Person p where p.BusinessEntityID = e.BusinessEntityID) as FirstName,
+    (select p.LastName from Person.Person p where p.BusinessEntityID = e.BusinessEntityID) as LastName,
+    e.BusinessEntityID,
+    (select COUNT(RateChangeDate) 
+     from HumanResources.EmployeePayHistory eph 
+     where eph.BusinessEntityID = e.BusinessEntityID) as PayRevisions
+from HumanResources.Employee e
+where (select COUNT(RateChangeDate) 
+       from HumanResources.EmployeePayHistory eph 
+       where eph.BusinessEntityID = e.BusinessEntityID) > 1
+
+--20.Find the personal details with address and address type
+--(hint: Business Entiry Address , Address, Address type) 
+
+select * from Person.BusinessEntityAddress
+Select * from Person.Address
+select * from Person.AddressType
+select* from Person.Person
+
+select
+CONCAT_WS(' ',p.FirstName,p.LastName)as Employee_Name,
+a.AddressLine1  Address,
+at.Name  Address_Type
+from person.person p
+join Person.BusinessEntityAddress ba
+on p.BusinessEntityID=ba.BusinessEntityID
+join Person.Address a
+on a.AddressID=ba.AddressID
+join Person.AddressType at
+on at.AddressTypeID=ba.AddressTypeID
+
+
+
+
+
+--21.	Find the name of employees working in group of North America territory ### 
+select TerritoryID,
+(select concat(' ',FirstName,LastName)from Person.Person p where p.BusinessEntityID=sp.BusinessEntityID)Emp_Name,
+(select name  from Sales.SalesTerritory st where  st.TerritoryID=sp.TerritoryID) TerritoryName,
+(select [Group] from Sales.SalesTerritory st1 where st1.TerritoryID=sp.TerritoryID)Group_NAme,SalesLastYear,SalesQuota
+from Sales.SalesPerson sp WHERE sp.TerritoryID IN (
+    SELECT TerritoryID 
+    FROM Sales.SalesTerritory 
+    WHERE [Group] IN ('North America'))
+
+--22.	Find the employee whose payment is revised for more than once 
+
+SELECT e.BusinessEntityID, p.FirstName, p.LastName, COUNT(eph.RateChangeDate) AS PayRevisions
+FROM HumanResources.EmployeePayHistory eph
+JOIN HumanResources.Employee e 
+ON eph.BusinessEntityID = e.BusinessEntityID
+JOIN Person.Person p 
+ON e.BusinessEntityID = p.BusinessEntityID
 GROUP BY e.BusinessEntityID, p.FirstName, p.LastName
+HAVING COUNT(eph.RateChangeDate) > 1;
+
+--23.display the personal details of  employee whose payment is revised for more than once.
+select * from Person.Address
+select * from Person.Person
+Select * from HumanResources.Employee
+
+SELECT p.BusinessEntityID, p.FirstName, p.LastName, p.EmailPromotion, e.Gender, e.JobTitle, 
+       eph.PayFrequency, COUNT(eph.RateChangeDate) AS PayRevisions
+FROM HumanResources.EmployeePayHistory eph
+JOIN HumanResources.Employee e ON eph.BusinessEntityID = e.BusinessEntityID
+JOIN Person.Person p ON e.BusinessEntityID = p.BusinessEntityID
+GROUP BY p.BusinessEntityID, p.FirstName, p.LastName, p.EmailPromotion, e.Gender, e.JobTitle, eph.PayFrequency
 HAVING COUNT(eph.RateChangeDate) > 1;
 
+--24.	find the duration of payment revision on every interval 
+--(inline view) Output must be as given format 
+--## revised time – count of revised salries 
+--## duration – last duration of revision 
+--e.g there are two revision date 01-01-2022 and revised in 01-01-2024   so duration here is 2years 
+
+select * from HumanResources.Employee
+select * from HumanResources.EmployeePayHistory
+
+ SELECT p.FirstName, p.LastName, SalaryRevisions.RevisedTime, 
+       DATEDIFF(YEAR, SalaryRevisions.FirstRevisionDate, SalaryRevisions.LastRevisionDate) AS Duration
+FROM (
+    SELECT eph.BusinessEntityID, 
+           COUNT(eph.RateChangeDate)  RevisedTime, 
+           MIN(eph.RateChangeDate)  FirstRevisionDate, 
+           MAX(eph.RateChangeDate)  LastRevisionDate
+    FROM HumanResources.EmployeePayHistory eph
+    GROUP BY eph.BusinessEntityID
+) AS SalaryRevisions
+JOIN Person.Person p ON p.BusinessEntityID = SalaryRevisions.BusinessEntityID
+ORDER BY RevisedTime DESC
+
+
+
+--25.check if any employee from jobcandidate table is having any payment revisions 
+select * from HumanResources.JobCandidate
+select * from HumanResources.Employee
+select * from HumanResources.EmployeePayHistory
+
+
+select * from HumanResources.JobCandidate j where j.BusinessEntityID
+in(select BusinessEntityID from HumanResources.Employee e where e.BusinessEntityID in 
+(select eph.BusinessEntityID from HumanResources.EmployeePayHistory eph group by eph.BusinessEntityID
+having COUNT(eph.RateChangeDate)>0))
+
+
+
+---26.check the department having more salary revision 
+select * from HumanResources.Department
+select * from HumanResources.EmployeeDepartmentHistory
+select * from HumanResources.EmployeePayHistory
+
+SELECT   d.Name AS DepartmentName, COUNT(eph.RateChangeDate) AS TotalSalaryRevisions
+from HumanResources.EmployeePayHistory eph
+join HumanResources.EmployeeDepartmentHistory edh 
+    on eph.BusinessEntityID = edh.BusinessEntityID
+join HumanResources.Department d 
+    on edh.DepartmentID = d.DepartmentID
+group by d.Name
+order by TotalSalaryRevisions DESC;
+
+--27.check the employee whose payment is not yet revised 
+select p.FirstName from Person.Person p where p.BusinessEntityID =
+(select e.BusinessEntityID from HumanResources.Employee e where e.BusinessEntityID
+not in (
+select distinct eph.BusinessEntityID from HumanResources.EmployeePayHistory eph)
+)--output is zero rows(query for verification purpose)
+
+select e.BusinessEntityID, concat_ws(' ',p.FirstName, p.LastName)as EmployeeName
+from HumanResources.Employee e
+join Person.Person p 
+on e.BusinessEntityID = p.BusinessEntityID
+where e.BusinessEntityID not in 
+(select distinct BusinessEntityID from HumanResources.EmployeePayHistory);
+
+--28.find the job title having more revised payments 
+
+select distinct(e.JobTitle), count(eph.RateChangeDate)  TotalSalaryRevisions
+from HumanResources.EmployeePayHistory eph
+join HumanResources.Employee e 
+    on eph.BusinessEntityID = e.BusinessEntityID
+group by e.JobTitle
+having count(eph.RateChangeDate)>1
+order by TotalSalaryRevisions desc;
+
+-------------------------------Inline View---------------------------------------------
+SELECT JobTitle, TotalSalaryRevisions
+FROM (
+    SELECT e.JobTitle, COUNT(eph.RateChangeDate) AS TotalSalaryRevisions
+    FROM HumanResources.EmployeePayHistory eph
+    JOIN HumanResources.Employee e 
+        ON eph.BusinessEntityID = e.BusinessEntityID
+    GROUP BY e.JobTitle
+) AS SalaryRevisions
+WHERE TotalSalaryRevisions > 1
+ORDER BY TotalSalaryRevisions DESC
+
+
+--29.find the employee whose payment is revised in shortest duration (inline view) 
+select BusinessEntityID, FirstName, LastName, min(datediff(day,StartDate, EndDate)) 
+as ShortestRevisionDuration
+from (
+    select e.BusinessEntityID, p.FirstName, p.LastName, eph.StartDate, eph.EndDate
+    from HumanResources.EmployeeDepartmentHistory eph
+    join HumanResources.Employee e on eph.BusinessEntityID = e.BusinessEntityID
+    join Person.Person p on e.BusinessEntityID = p.BusinessEntityID
+) as PaymentRevisions
+group by  BusinessEntityID, FirstName, LastName;
+
+--30.find the colour wise count of the product (tbl: product)
+select Color, count(ProductID) ProductCount
+from Production.Product
+where Color is not null
+group by Color
+order  by ProductCount desc;
+
+--31.find out the product who are not in position to sell (hint: check the sell start and end date)
+select * from Production.Product
+
+select distinct ProductID, Name, SellStartDate, SellEndDate
+from Production.Product where (SellEndDate is not null and SellEndDate < GETDATE()) 
+ or SellStartDate is null
+
+--32.find the class wise, style wise average standard cost  
+
+select class Class,style Style,avg(StandardCost)Avg_Cost from Production.Product where
+class is not null and Style is not null
+group by Class,Style 
+order by Avg_Cost 
+
+
+--33.check colour wise standard cost 
+ select * from Production.Product
+
+ select color Color,avg(StandardCost)Color_AvgCost from Production.Product
+ where color is not null 
+ group by Color
+ order by Color_AvgCost
+
+ --34.find the product line wise standard cost 
+ select Productline Product_line,avg(StandardCost)P_Std from Production.Product
+ where ProductLine is not null
+ group by ProductLine
+ order by P_Std
+
+ --35.Find the state wise tax rate (hint: Sales.SalesTaxRate, Person.StateProvince) 
+
+SELECT sp.Name AS StateName, sp.StateProvinceCode, str.TaxRate
+FROM Sales.SalesTaxRate str
+JOIN Person.StateProvince sp 
+    ON str.StateProvinceID = sp.StateProvinceID
+ORDER BY sp.Name  
+
+--Q36. Find the department wise count of employees 
+select* from HumanResources.Employee
+select*from HumanResources.Department
+select*from HumanResources.EmployeeDepartmentHistory
+
+select d.Name as DepartmentName,count(e.BusinessEntityID) as EmployeeCount
+from HumanResources.Employee e
+join HumanResources.EmployeeDepartmentHistory edh
+on e.BusinessEntityID=edh.BusinessEntityID
+join HumanResources.Department d
+on d.DepartmentID=edh.DepartmentID
+group by d.Name
+
+--37.Find the department which is having more employees 
+
+SELECT d.DepartmentID, d.Name AS DepartmentName, COUNT(e.BusinessEntityID) AS EmployeeCount
+FROM HumanResources.Employee e
+JOIN HumanResources.Department d ON e.BusinessEntityID = d.DepartmentID
+GROUP BY d.DepartmentID, d.Name
+ORDER BY EmployeeCount DESC
+
+--38.Find the job title having more employees 
+Select * from HumanResources.Employee
+select*from HumanResources.Department
+
+select count(BusinessEntityID)as EmployeeCount,JobTitle from  HumanResources.Employee
+group by JobTitle
+order by EmployeeCount desc
+
+--39.Check if there is mass hiring of employees on single day 
+Select * from HumanResources.Employee
+
+select  Hiredate, count(BusinessEntityID)Employee_count  From HumanResources.Employee
+group by HireDate
+Having count(BusinessEntityID)>1
+Order by Employee_count desc
+
+--40.Which product is purchased more? (purchase order details) 
+select * from Purchasing.PurchaseOrderDetail
+select * from Production.Product
+
+
+SELECT  p.ProductID, p.Name AS Product_Name, SUM(pd.OrderQty) AS TotalQuantityPurchased
+FROM Purchasing.PurchaseOrderDetail pd
+JOIN Production.Product p ON p.ProductID = pd.ProductID
+GROUP BY p.ProductID, p.Name
+ORDER BY TotalQuantityPurchased DESC
+
+--41.Find the territory wise customers count   (hint: customer) 
+
+select * from Sales.Customer
+SELECT TerritoryID, COUNT(CustomerID) AS CustomerCount
+FROM Sales.Customer
+GROUP BY TerritoryID
+ORDER BY CustomerCount DESC;
+
+--42.Which territory is having more customers (hint: customer) 
+
+SELECT  TerritoryID, COUNT(CustomerID) AS CustomerCount
+FROM Sales.Customer
+GROUP BY TerritoryID
+ORDER BY CustomerCount DESC
+
+--43.Which territory is having more stores (hint: customer) 
+ 
+ SELECT  TerritoryID, COUNT(StoreID) AS Store_Count
+FROM Sales.Customer
+GROUP BY TerritoryID
+ORDER BY Store_Count DESC
+
+--44. Is there any person having more than one credit card (hint: PersonCreditCard) 
+select*from Person.Person
+select * from Sales.PersonCreditCard 
+
+select CONCAT_WS(' ',p.FirstName,p.LastName)as PersonName,COUNT(pc.CreditCardID)as CreditCardCount
+from Person.Person p
+join sales.PersonCreditCard pc
+on p.BusinessEntityID=pc.BusinessEntityID
+group by p.FirstName,p.LastName
+having count(pc.CreditCardID)>1
+
+--45.Find the product wise sale price (sales order details)		
+select * from Production.Product
+select * from sales.SalesOrderDetail
+
+SELECT p.ProductID, p.Name AS ProductName, 
+       SUM(sod.OrderQty * sod.UnitPrice) AS TotalSalesPrice
+FROM Sales.SalesOrderDetail sod
+JOIN Production.Product p ON sod.ProductID = p.ProductID
+GROUP BY p.ProductID, p.Name
+ORDER BY TotalSalesPrice DESC
+
+--
+--46Find the total values for line total product having maximum order 
+
+
+
+
+--48.Calculate the age of employees 
+select* from HumanResources.Employee
+
+SELECT BusinessEntityID, BirthDate, 
+       DATEDIFF(YEAR, BirthDate, GETDATE()) - 
+       CASE 
+           WHEN (MONTH(BirthDate) > MONTH(GETDATE())) 
+                OR (MONTH(BirthDate) = MONTH(GETDATE()) AND DAY(BirthDate) > DAY(GETDATE())) 
+           THEN 1 ELSE 0 
+       END AS Age
+FROM HumanResources.Employee;
+-------other way
+select concat_ws(' ',p.FirstName,p.LastName)as EmployeeName,
+year(getdate())-year(e.BirthDate)as Age
+from HumanResources.Employee e
+join Person.Person p
+on e.BusinessEntityID=p.BusinessEntityID
+
+
+--49.Calculate the year of experience of the employee based on hire date
+
+
+
+select concat_ws(' ',p.FirstName,p.LastName)as EmployeeName,
+year(getdate())-year(e.HireDate)Experience
+from HumanResources.Employee e
+join Person.Person p
+on e.BusinessEntityID=p.BusinessEntityID
+
+--50.Find the age of employee at the time of joining 
+
+select  e.BusinessEntityID,concat_ws(' ',p.FirstName,p.LastName)as EmployeeName,
+year(e.HireDate)-year(e.BirthDate)Age_at_joining
+from HumanResources.Employee e
+join Person.Person p
+on e.BusinessEntityID=p.BusinessEntityID
+
+SELECT BusinessEntityID,BirthDate, HireDate, 
+    DATEDIFF(YEAR, BirthDate, HireDate) AS AgeAtJoining
+FROM HumanResources.Employee
+
+--51.Find the average age of male and female
+select * from HumanResources.Employee
+
+select Gender,Avg(datediff(YEAR,birthdate,GETDATE()))Avg_Age from HumanResources.Employee
+group by Gender
+
+ --52.Which product is the oldest product as on the date (refer  the product sell start date) 
+ select * from Production.Product
+
+ 
+ select top 1 name,
+ max(year(getdate())-year(SellStartDate))as productage
+ from Production.Product
+ group by Name
